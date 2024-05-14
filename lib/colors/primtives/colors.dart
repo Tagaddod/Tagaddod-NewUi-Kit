abbdd8074b6bcda0dc09882e52d3699f9881aa16
Kholy <abdelrahman.elkholy@tagaddod.com>
2024-05-14 14:52:49 +0300

    added reds, oranges, purples

--- lib/colors/primtives/colors.dart
+++ lib/colors/primtives/colors.dart
@@ -1,1 +1,58 @@
-class Colors {}
+import 'dart:ui';
+
+class Colors {
+  ///RED COLORS
+  static const colorRed100 = Color(0xFFFEF9F9);
+  static const colorRed200 = Color(0xFFFEF6F6);
+  static const colorRed300 = Color(0xFFFDF3F3);
+  static const colorRed400 = Color(0xFFFDF0F1);
+  static const colorRed500 = Color(0xFFFCEAEB);
+  static const colorRed600 = Color(0xFFFCE6E6);
+  static const colorRed700 = Color(0xFFFBE1E2);
+  static const colorRed800 = Color(0xFFFAD7D8);
+  static const colorRed900 = Color(0xFFF8CCCE);
+  static const colorRed1000 = Color(0xFFF5B6B8);
+  static const colorRed1100 = Color(0xFFEC7378);
+  static const colorRed1200 = Color(0xFFD51E25);
+  static const colorRed1300 = Color(0xFFBF1B21);
+  static const colorRed1400 = Color(0xFF8D1419);
+  static const colorRed1500 = Color(0xFF660E12);
+  static const colorRed1600 = Color(0xFF3A080A);
+
+  ///ORANGES
+  static const colorOrange100 = Color(0xFFFEFAF6);
+  static const colorOrange200 = Color(0xFFFEF8F2);
+  static const colorOrange300 = Color(0xFFFEF4EA);
+  static const colorOrange400 = Color(0xFFFEF0E1);
+  static const colorOrange500 = Color(0xFFFEEBD8);
+  static const colorOrange600 = Color(0xFFFEE7CF);
+  static const colorOrange700 = Color(0xFFFEE3C5);
+  static const colorOrange800 = Color(0xFFFED8AB);
+  static const colorOrange900 = Color(0xFFFECF90);
+  static const colorOrange1000 = Color(0xFFFEBA2F);
+  static const colorOrange1100 = Color(0xFFC78E00);
+  static const colorOrange1200 = Color(0xFF8F6500);
+  static const colorOrange1300 = Color(0xFF805A00);
+  static const colorOrange1400 = Color(0xFF5E4200);
+  static const colorOrange1500 = Color(0xFF442E00);
+  static const colorOrange1600 = Color(0xFF261800);
+
+  ///PURPLES
+  static const colorPurple100 = Color(0xFFFCFAFF);
+  static const colorPurple200 = Color(0xFFFBF7FF);
+  static const colorPurple300 = Color(0xFFF9F4FF);
+  static const colorPurple400 = Color(0xFFF6EFFF);
+  static const colorPurple500 = Color(0xFFF3EAFF);
+  static const colorPurple600 = Color(0xFFF2E7FF);
+  static const colorPurple700 = Color(0xFFEFE2FF);
+  static const colorPurple800 = Color(0xFFE9D7FF);
+  static const colorPurple900 = Color(0xFFE3CDFF);
+  static const colorPurple1000 = Color(0xFFD7B8FF);
+  static const colorPurple1100 = Color(0xFFB57CFF);
+  static const colorPurple1200 = Color(0xFF8C34FF);
+  static const colorPurple1300 = Color(0xFF7B18FD);
+  static const colorPurple1400 = Color(0xFF5B12BC);
+  static const colorPurple1500 = Color(0xFF410D87);
+  static const colorPurple1600 = Color(0xFF25074D);
+
+}
