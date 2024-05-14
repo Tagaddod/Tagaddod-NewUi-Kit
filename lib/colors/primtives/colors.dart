d1a4dc4dfe90abc2675f8de953cbfb56873a4b9d
Ebtsam <ebtsamosama25@gmail.com>
2024-05-14 13:59:55 +0300

    add primitive colors

--- lib/colors/primtives/colors.dart
+++ lib/colors/primtives/colors.dart
@@ -1,1 +1,58 @@
-class Colors {}
+import 'package:flutter/material.dart';
+class Colors {
+  static const colorBlue100 = Color(0xffFAFAFF);
+  static const colorBlue200 = Color(0xffF8F8FF);
+  static const colorBlue300 = Color(0xffF4F4FF);
+  static const colorBlue400 = Color(0xffF1F2FF);
+  static const colorBlue500 = Color(0xffEBECFF);
+  static const colorBlue600 = Color(0xffE9EAFF);
+  static const colorBlue700 = Color(0xffE4E6FF);
+  static const colorBlue800 = Color(0xffD9DCFE);
+  static const colorBlue900 = Color(0xffD0D4FE);
+  static const colorBlue1000 = Color(0xffBCC2FE);
+  static const colorBlue1100 = Color(0xff828EFE);
+  static const colorBlue1200 = Color(0xff3959FE);
+  static const colorBlue1300 = Color(0xff0B48FE);
+  static const colorBlue1400 = Color(0xff0033C2);
+  static const colorBlue1500 = Color(0xff00238F);
+  static const colorBlue1600 = Color(0xff001157);
+
+
+  static const colorGray100 = Color(0xffFFFFFF);
+  static const colorGray200 = Color(0xffFCFCFD);
+  static const colorGray300 = Color(0xffF9F9FA);
+  static const colorGray400 = Color(0xffF7F7F8);
+  static const colorGray500 = Color(0xffF1F1F3);
+  static const colorGray600 = Color(0xffEAEAED);
+  static const colorGray700 = Color(0xffE0E2E6);
+  static const colorGray800 = Color(0xffCFD1D8);
+  static const colorGray900 = Color(0xffC7C8D1);
+  static const colorGray1000 = Color(0xffADAFBD);
+  static const colorGray1100 = Color(0xff7D8096);
+  static const colorGray1200 = Color(0xff686C83);
+  static const colorGray1300 = Color(0xff55596C);
+  static const colorGray1400 = Color(0xff414353);
+  static const colorGray1500 = Color(0xff2A2C37);
+  static const colorGray1600 = Color(0xff16161D);
+
+
+  static const colorGreen100 = Color(0xffEFFFF4);
+  static const colorGreen200 = Color(0xffE8FFEE);
+  static const colorGreen300 = Color(0xffD7FFE3);
+  static const colorGreen400 = Color(0xffCEFFDD);
+  static const colorGreen500 = Color(0xffB1FFCB);
+  static const colorGreen600 = Color(0xffA0FFC1);
+  static const colorGreen700 = Color(0xff8DFFB7);
+  static const colorGreen800 = Color(0xff29FE94);
+  static const colorGreen900 = Color(0xff00F68B);
+  static const colorGreen1000 = Color(0xff00E380);
+  static const colorGreen1100 = Color(0xff00AE61);
+  static const colorGreen1200 = Color(0xff007D44);
+  static const colorGreen1300 = Color(0xff00703C);
+  static const colorGreen1400 = Color(0xff00522B);
+  static const colorGreen1500 = Color(0xff003A1D);
+  static const colorGreen1600 = Color(0xff00200D);
+
+
+  
+}
