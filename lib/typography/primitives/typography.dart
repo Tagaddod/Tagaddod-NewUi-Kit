5a8ed5e40caa289dcc70c345a37da18733eaec24
Kholy <abdelrahman.elkholy@tagaddod.com>
2024-05-14 16:43:34 +0300

    added typography primitive class and required fonts

---
+++ lib/typography/primitives/typography.dart
@@ -0,0 +1,25 @@
+import 'dart:ui';
+
+class Typography {
+  static const fontFamilySansEnglish = 'Outfit';
+  static const fontFamilySansArabic = 'DIN Next LT W23';
+
+  static const fontWeightRegular = FontWeight.w400;
+  static const fontWeightMedium = FontWeight.w500;
+  static const fontWeightSemiBold = FontWeight.w600;
+  static const fontWeightBold = FontWeight.w700;
+
+  static const fontSize250 = 10.0;
+  static const fontSize300 = 12.0;
+  static const fontSize350 = 14.0;
+  static const fontSize400 = 16.0;
+  static const fontSize450 = 18.0;
+  static const fontSize500 = 20.0;
+  static const fontSize550 = 22.0;
+  static const fontSize600 = 24.0;
+  static const fontSize700 = 28.0;
+  static const fontSize900 = 36.0;
+  static const fontSize1000 = 40.0;
+  static const fontSize1200 = 48.0;
+  static const fontSize1400 = 56.0;
+}
