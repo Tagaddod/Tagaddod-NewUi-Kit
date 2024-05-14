8d46da61668936a22746b0576f7152a70076f5c6
Ebtsam <ebtsamosama25@gmail.com>
2024-05-14 16:17:39 +0300

    add bg colors

---
+++ lib/colors/semantic/bg_colors.dart
@@ -0,0 +1,46 @@
+import 'dart:ui';
+
+import 'package:tagaddod_ui_kit/colors/primtives/colors.dart';
+
+class BgColors {
+  static const colorBg = Colors.colorGray400;
+  static const colorBgLight = Colors.colorGray300;
+
+  static const colorBgSurface = Colors.colorGray100;
+  static const colorBgSurfaceHover = Colors.colorGray300;
+  static const colorBgSurfaceActive = Colors.colorGray500;
+  static const colorBgSurfaceDisabled = Colors.colorGray700;
+
+  static const colorBgFill = Colors.colorGray100;
+  static const colorBgFillHover = Colors.colorGray300;
+  static const colorBgFillActive = Colors.colorGray500;
+  static const colorBgFillDisabled = Colors.colorGray700;
+
+  static const colorBgFillBrand = Colors.colorBlue1200;
+  static const colorBgFillBrandHover = Colors.colorBlue1300;
+  static const colorBgFillBrandActive = Colors.colorBlue1400;
+  static const colorBgFillBrandSecondary = Colors.colorBlue400;
+
+  static const colorBgFillSuccess = Colors.colorGreen1100;
+  static const colorBgFillSuccessHover = Colors.colorGreen1200;
+  static const colorBgFillSuccessActive = Colors.colorGreen1300;
+  static const colorBgFillSuccessSecondary = Colors.colorGreen400;
+
+  static const colorBgFillCritical = Colors.colorRed1200;
+  static const colorBgFillCriticalHover = Colors.colorRed1300;
+  static const colorBgFillCriticalActive = Colors.colorRed1400;
+  static const colorBgFillCriticalSecondary = Colors.colorRed400;
+
+  static const colorBgFillWarning = Colors.colorOrange1000;
+  static const colorBgFillWarningHover = Colors.colorOrange1100;
+  static const colorBgFillWarningActive = Colors.colorOrange1200;
+  static const colorBgFillWarningSecondary = Colors.colorOrange400;
+
+
+  static const colorBgFillMagic= Colors.colorPurple1200;
+  static const colorBgFillMagicHover = Colors.colorPurple1300;
+  static const colorBgFillMagicActive = Colors.colorPurple1400;
+  static const colorBgFillMagicSecondary = Colors.colorPurple400;
+
+  static Color colorBgByOverlay20=Colors.colorGray100.withOpacity(.2);
+}
