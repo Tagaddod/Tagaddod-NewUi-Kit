104b8d23c9a9f2274d2a8e17f17ab24817e7306e
Ebtsam <ebtsamosama25@gmail.com>
2024-05-15 10:34:26 +0300

    add text colors

---
+++ lib/colors/semantic/text_colors.dart
@@ -0,0 +1,40 @@
+import 'package:tagaddod_ui_kit/colors/primtives/colors.dart';
+
+class TextColors{
+  static const colorText = Colors.colorGray1200;
+  static const colorTextSecondary = Colors.colorGray1100;
+  static const colorTextDisabled = Colors.colorGray1000;
+
+
+  static const colorTextLink = Colors.colorBlue1200;
+  static const colorTextLinkHover = Colors.colorBlue1300;
+  static const colorTextLinkActive = Colors.colorBlue1400;
+
+
+  static const colorTextOnBgFill = Colors.colorGray100;
+  static const colorTextOnBgFillHover = Colors.colorGray300;
+  static const colorTextOnBgFillActive= Colors.colorGray500;
+  static const colorTextOnBgFillDisabled= Colors.colorGray100;
+
+
+  static const colorTextSuccess = Colors.colorGreen1100;
+  static const colorTextSuccessHover = Colors.colorGreen1200;
+  static const colorTextSuccessActive= Colors.colorGreen1300;
+  static const colorTextSuccessSecondary= Colors.colorGreen1200;
+
+
+  static const colorTextCritical = Colors.colorRed1200;
+  static const colorTextCriticalHover = Colors.colorRed1300;
+  static const colorTextCriticalActive= Colors.colorRed1400;
+  static const colorTextCriticalSecondary= Colors.colorRed1300;
+
+
+  static const colorTextWarning = Colors.colorOrange1200;
+  static const colorTextWarningSecondary= Colors.colorOrange1100;
+
+
+  static const colorTextMagic = Colors.colorPurple1200;
+  static const colorTextMagicHover = Colors.colorPurple1300;
+  static const colorTextMagicActive= Colors.colorPurple1400;
+  static const colorTextMagicSecondary= Colors.colorPurple1300;
+}
