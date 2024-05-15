68924d4666e89a5d1bb794466c0010c805982589
Ebtsam <ebtsamosama25@gmail.com>
2024-05-15 11:29:42 +0300

    add icons & border colors

---
+++ lib/colors/semantic/border_colors.dart
@@ -0,0 +1,22 @@
+import 'package:tagaddod_ui_kit/colors/primtives/colors.dart';
+
+class BorderColors{
+  static const colorBorder = Colors.colorGray700;
+  static const colorBorderSecondary = Colors.colorGray500;
+  static const colorBorderTertiary = Colors.colorGray300;
+  static const colorBorderHover = Colors.colorGray1100;
+  static const colorBorderActive = Colors.colorGray1300;
+
+  static const colorBorderBrand= Colors.colorBlue1200;
+  static const colorBorderBrandHover = Colors.colorBlue1300;
+  static const colorBorderBrandActive = Colors.colorBlue1400;
+
+  static const colorBorderSuccess = Colors.colorGreen1100;
+  static const colorBorderCritical = Colors.colorRed1200;
+  static const colorBorderWarning = Colors.colorOrange1000;
+
+  static const colorBorderMagic = Colors.colorPurple1200;
+  static const colorBorderMagicHover = Colors.colorPurple1300;
+  static const colorBorderMagicActive= Colors.colorPurple1400;
+
+}
---
+++ lib/colors/semantic/icon_colors.dart
@@ -0,0 +1,33 @@
+import 'package:tagaddod_ui_kit/colors/primtives/colors.dart';
+
+class IconColors{
+  static const colorIcon = Colors.colorGray1200;
+  static const colorIconSecondary = Colors.colorGray1100;
+  static const colorIconDisabled = Colors.colorGray1000;
+
+
+  static const colorIconLink = Colors.colorBlue1200;
+  static const colorIconLinkHover = Colors.colorBlue1300;
+  static const colorIconLinkActive = Colors.colorBlue1400;
+
+
+  static const colorIconOnBgFill = Colors.colorGray100;
+  static const colorIconOnBgFillHover = Colors.colorGray300;
+  static const colorIconOnBgFillActive= Colors.colorGray500;
+  static const colorIconOnBgFillDisabled= Colors.colorGray100;
+
+
+  static const colorIconSuccess = Colors.colorGreen1100;
+  static const colorIconSuccessHover = Colors.colorGreen1200;
+  static const colorIconSuccessActive= Colors.colorGreen1300;
+
+
+  static const colorIconCritical = Colors.colorRed1200;
+  static const colorIconCriticalHover = Colors.colorRed1300;
+  static const colorIconCriticalActive= Colors.colorRed1400;
+
+
+  static const colorIconMagic = Colors.colorPurple1200;
+  static const colorIconMagicHover = Colors.colorPurple1300;
+  static const colorIconMagicActive= Colors.colorPurple1400;
+}
