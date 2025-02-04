2c4f7e0f235a9fd006e34eaf66c12ad31dd7bafc
alisabry72 <ali.sabry@tagaddod.com>
2025-02-04 12:26:25 +0200

    push app filled button

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tagaddod_ui_kit/colors/semantic/text_colors.dart';
 import 'package:tagaddod_ui_kit/widgets/app_filled_button.dart';
 
-void main() {
+void main() async {
+  await ScreenUtil.ensureScreenSize();
   runApp(const MyApp());
 }
 
@@ -11,18 +13,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Scaffold(
-          body: Center(
-              child: AppFilledButton.large(
-        btnText: "Tagaddod",
-        textColor: TextColors.colorTextMagic,
-      ))),
-    );
+    return ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        builder: (_, child) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            home: Scaffold(
+                body: Center(
+                    child: AppFilledButton.large(
+              btnText: "Tagaddod",
+              textColor: TextColors.colorTextMagic,
+            ))),
+          );
+        });
   }
 }
